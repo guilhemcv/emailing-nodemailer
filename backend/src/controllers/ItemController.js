4186388fd5835757523fcbef2f2fa0a4b3f59003
Guilhem SEYVET <guilhemcvdev@outlook.com>
2022-06-16 12:18:21 +0200

    adding sendMail function in the itemController file

--- backend/src/controllers/ItemController.js
+++ backend/src/controllers/ItemController.js
@@ -1,4 +1,6 @@
+const nodemailer = require("nodemailer");
 const models = require("../models");
+require("dotenv").config();
 
 class ItemController {
   static browse = (req, res) => {
@@ -76,6 +78,39 @@ class ItemController {
       .catch((err) => {
         console.error(err);
         res.sendStatus(500);
+      });
+  };
+
+  static sendMail = (req, res) => {
+    const { name, surname, phone, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_SENDIN,
+      port: process.env.SMTP_PORT_SENDIN,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_SENDIN_USER,
+        pass: process.env.SMTP_SENDIN_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: "hello@gmail.com",
+      to: "guilhem.seyvet@wildcodeschool.com",
+      subject: "New message from contact form",
+      text: `${message} \n\n Phone: ${phone} \n\n Name: ${name} \n\n Surname: ${surname} \n\n Email: ${email}`,
+      html: `<p>${message}</p> <p>Phone: ${phone}</p> <p>Name: ${name}</p> <p>Surname: ${surname}</p> <p>Email: ${email}</p>`,
+    };
+
+    return transporter
+      .sendMail(mailOptions)
+      .then((info) => {
+        console.warn(info);
+        res.status(200).send("Message sent");
+      })
+      .catch((err) => {
+        console.warn(err);
+        res.status(500).send("Something went wrong");
       });
   };
 }
